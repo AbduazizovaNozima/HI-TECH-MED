529c606249894bf1f5e9f60a6db1365062ce3cd9
Nozima <abduazizovanozima7@gmail.com>
2025-03-19 20:18:42 +0500

    qwert

--- medical/settings.py
+++ medical/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bif2uql90gztjb!+u0n=cez_j%^=$6+hmn=5!_rt(xywxjqs^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
